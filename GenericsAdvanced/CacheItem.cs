5211e5c04171cfdaca659c7fb8ff1a94d4c62fd9
AT <AT@DESKTOP-AKM319L>
2024-09-09 23:28:50 +0500

    Add project files.

---
+++ GenericsAdvanced/CacheItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAdvanced
+{
+    internal class CacheItem<T>
+    {
+        public T Value { get; set; }
+
+        public DateTime ExpirationTime { get; set; }
+
+
+        public CacheItem(T value, TimeSpan expirationDuration)
+
+        {
+
+            Value = value;
+
+            ExpirationTime = DateTime.Now.Add(expirationDuration);
+
+        }
+
+
+        public bool IsExpired()
+
+        {
+
+            return DateTime.Now > ExpirationTime;
+
+        }
+    }
+}
---
+++ GenericsAdvanced/GenericCache.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAdvanced
+{
+    internal class GenericCache<T>
+    {
+
+        private Dictionary<string, CacheItem<T>> cache = new Dictionary<string, CacheItem<T>>();
+
+
+        public void AddItem(string key, T value, TimeSpan expirationDuration)
+
+        {
+
+            CacheItem<T> cacheItem = new CacheItem<T>(value, expirationDuration);
+
+            cache[key] = cacheItem;
+
+        }
+
+
+        public T GetItem(string key)
+
+        {
+
+            if (cache.TryGetValue(key, out CacheItem<T> cacheItem))
+
+            {
+
+                if (cacheItem.IsExpired())
+
+                {
+
+                    // Remove expired item from the cache
+
+                    cache.Remove(key);
+
+                    throw new InvalidOperationException("Attempted to retrieve an expired item from the cache.");
+
+                }
+
+
+                return cacheItem.Value;
+
+            }
+
+
+            throw new KeyNotFoundException($"Item with key '{key}' not found in the cache.");
+
+        }
+
+
+        public void Display()
+
+        {
+
+            Console.WriteLine("Current items in the cache:");
+
+            foreach (var kvp in cache)
+
+            {
+
+                Console.WriteLine($"Key: {kvp.Key}, Expiration Time: {kvp.Value.ExpirationTime}, Value: {kvp.Value.Value}");
+
+            }
+
+        }
+    }
+}
